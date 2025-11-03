006584b1a95e2cdd5654927308cf24fad92d3dc3
Abhishek Singh <abhisheksingh39867@gmail.com>
2025-11-03 19:45:20 +0530

    Initial commit project setup

---
+++ dazzling-screens/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { div } from "framer-motion/client";
+
+export default function DashboardPage(){
+    return(
+        <div className=" text-white text-4xl">Here is your Dashboard</div>
+    )
+}
---
+++ dazzling-screens/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Theater Booking System 🎭</h1>
+      <p className="text-lg">Book your favorite movies with ease!</p>
+      <button className="">Book Now</button>
+    </main>
+  );
+}
